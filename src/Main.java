d8784322413e34cae6ac77b9c2721c1c3cee67d3
MartaM096 <martam096@wp.pl>
2022-09-20 20:02:55 +0200

    added for each loop

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //Forloop
+        //ForEachLoop
         String[] cars=new String[] {"Audi", "BMW", "Mercedes", "Toyota"};
-        for (int i=0; i<4; i++){
-            System.out.println(cars[i]);
+        for (String car:cars){
+            System.out.println(car);
         }
     }
 }
